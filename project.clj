fb99941bf4368c4f309be5823e746a5db39f8fd0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-23 10:16:57 +0100

    Adding default description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pedestal-lab "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "pedestal learning playground"
+  :url "https://github.com/ardumont/pedestal-lab"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
