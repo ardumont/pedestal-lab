f4e60d7d5d49e4214d65d014c4a3214de58bb1a4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-09 09:56:55 +0200

    Adding comments to explain what's going on.

--- client/app/src/pedestal_app/app.cljs
+++ client/app/src/pedestal_app/app.cljs
@@ -17,25 +17,34 @@
             [io.pedestal.app.messages :as msg]
             [domina :as dom]))
 
+;; update the state via the count-model function
 (defn count-model [old-state message]
   (condp = (msg/type message)
     msg/init (:value message)
     :inc (inc old-state)))
 
-(defn render-value [r [_ _ old-value new-value] input-queue]
+;; the inputs are named here to help in understanding the sample but really, only the new-value is used!
+(defn render-value [renderer [_ _ old-value new-value] input-queue]
   (dom/destroy-children! (dom/by-id "content"))
   (dom/append! (dom/by-id "content")
                (str "<h1>" new-value " Hello Worlds</h1>")))
 
-(def count-app {:transform {:count {:init 0 :fn count-model}}})
+;; Application data flow to build the application.
+;; (Description of functions and inputs are separated from the execution strategy)
+(def count-app-flow {:transform {:count {:init 0 :fn count-model}}})
 
 (defn receive-input [input-queue]
   (p/put-message input-queue {msg/topic :count msg/type :inc})
   (.setTimeout js/window #(receive-input input-queue) 3000))
 
 (defn ^:export main []
-  (let [app (app/build count-app)
-        render-fn (push/renderer "content" [[:value [:**] render-value]])]
+  (let [;; build the application with the data flow
+        app (app/build count-app-flow)
+        ;; Compute the rendering function (which will be able to make senses of deltas)
+        ;; All changes of value concerning the "content" (dom) will be sent to the render-value function
+        render-fn (push/renderer "content" [[:value [:*] render-value]])]
+    ;; Renderer receives only delta of what has changed.
+    ;; This way, they can concentrate on small part and pieces, which makes them small and simple
     (render/consume-app-model app render-fn)
     (receive-input (:input app))
     (app/begin app)))
